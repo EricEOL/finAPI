3421cb32875fad8750e3f860438fa502eb2fb9fb
EricEOL <lopes.eric051@gmail.com>
2021-07-05 20:50:11 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/courses', (req, res) => {
-  const query = req.query;
-  console.log(query);
-  return res.json(["Curso 1","Curso 2","Curso 3"])
-})
+const customers = [];
 
-app.post('/courses', (req, res) => {
-  const body = req.body;
-  console.log(body);
-  return res.json(["Curso 1","Curso 2","Curso 3", "Curso 4"]);  
-})
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidv4();
+  
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
 
-app.put('/courses/:id', (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-  return res.json(["Curso 6","Curso 2","Curso 3", "Curso 4"]); 
-})
-
-app.patch('/courses/:id', (req, res) => {
-  return res.json(["Curso 6","Curso 7","Curso 3", "Curso 4"]); 
-})
-
-app.delete('/courses/:id', (req, res) => {
-  return res.json(["Curso 6","Curso 7","Curso 4"]); 
+  return res.status(201).send();
 })
 
 app.listen(3333, () => console.log('Nodeapp running'));
